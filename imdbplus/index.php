69cf4bba17914abfb4feee15d22f8706f449f13e
informatica64-tools <64447674+informatica64-tools@users.noreply.github.com>
2021-11-14 17:06:15 -0600

    Add files via upload

---
+++ imdbplus/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Buy Pelis</title>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<div class="header container">
+			<header>
+				<h1>
+					<span><a href="./">IMDBPLUS</a></abbr></span>
+				</h1>
+
+				<nav class="menu">
+				 	<ul>
+				 		<li><a href="./">Home</a></li>
+				 		<li><a href="movies.php">Movies</a></li>
+				 		<li><a href="series.php">Series</a></li>
+				 		<li><a href="categories.php">Categories</a>
+				 			<ul>
+				 				<li><a href="#">Terror</a></li>
+				 				<li><a href="#">Action</a></li>
+				 				<li><a href="#">Comedy</a></li>
+				 				<li><a href="#">Fantasy</a></li>
+				 				<li><a href="#">Science</a></li>
+				 				<li><a href="#">Suspense</a></li>
+				 			</ul>
+				 		</li>
+				 		<li><a href="kids.php">Kids</a></li>
+				 	</ul>
+				</nav>
+
+				<div class="search">
+					<i class="fas fa-search"></i>
+				</div>
+
+				<div class="start">
+					<ul>
+						<li><a href="subscription/subscribe.php">SUBSCRIBE</a></li>
+						<li><a href="register/register.php">REGISTER</a></li>
+					</ul>
+				</div>
+			</header>
+	</div>
+
+
+
+	<div class="container">
+
+		<div class="floaiting-cover">
+			<ul class="menu1">
+				<li><a href="#bruce lee"><img src="images/bruce lee.jpeg"></a></li>
+		        <li><a href="#jackiechan"><img src="images/jackiechan.jpeg"></a></li>
+		        <li><a href="#hackers1995"><img src="images/hackers1995.jpeg"></a></li>
+		        <li><a href="#matrix"><img src="images/matrix.jpg"></a></li>
+		        <li><a href="#sankypanky"><img src="images/sankypanky.jpeg"></a></li>
+			</ul>
+		</div>
+
+		<div class="other-covers">
+			<ul>
+				<li><a src="covers/#"><img src="covers/#"></a></li>
+				<li><a src="covers/#"><img src="covers/#"></a></li>
+				<li><a src="covers/#"><img src="covers/#"></a></li>
+				<li><a src="covers/#"><img src="covers/#"></a></li>
+				<li><a src="covers/#"><img src="covers/#"></a></li>
+				<li><a src="covers/#"><img src="covers/#"></a></li>
+				<li><a src="covers/#"><img src="covers/#"></a></li>
+				<li><a src="covers/#"><img src="covers/#"></a></li>
+				<li><a src="covers/#"><img src="covers/#"></a></li>
+				<li><a src="covers/#"><img src="covers/#"></a></li>
+			</ul>
+		</div>
+
+
+		<div class="other-covers">
+			<h2>Las más vistas</h2>
+			<ul>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+			</ul>
+		</div>
+
+
+		<div class="other-covers">
+			<h2>Títulos recomendados<h2>
+			<ul>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+				<li><a src="covers/#las-mas-vistas"><img src="covers/"></a></li>
+			</ul>
+		</div>
+
+	</div>
+
+	<footer>
+		
+	</footer>
+
+
+</body>
+</html>
+
